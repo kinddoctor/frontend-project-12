a50c658a86ee91a30b33fa30d9bee982b1f5f230
kinddoctor <tata-yagodina@yandex.ru>
2024-09-05 16:34:45 +0500

    restructure project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,12 @@
+import ErrorPage from './ErrorPage';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit
-          <code>src/App.jsx</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        This is header
       </header>
+      <ErrorPage />
     </div>
   );
 }
---
+++ frontend/src/pages/404.jsx
@@ -0,0 +1,13 @@
+function ErrorPage() {
+  return (
+    <div className="text-center">
+      <img src="./img/404.svg" alt="Page is not founded - error 404" className="img-fluid h-50" />
+      <h1 className="h3">Страница не найдена</h1>
+      <p>
+        Давайте вернемся на главную страницу
+      </p>
+    </div>
+  );
+}
+
+export default ErrorPage;
