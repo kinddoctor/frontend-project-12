8e4f3629bc4219fa28c1cec8d4de119e6fd8391f
kinddoctor <tata-yagodina@yandex.ru>
2024-09-12 21:20:28 +0500

    fix image path

--- frontend/src/pages/404.jsx
+++ frontend/src/pages/404.jsx
@@ -1,10 +1,11 @@
 function ErrorPage() {
   return (
     <div className="text-center">
-      <img src="./img/404.svg" alt="Page is not founded - error 404" className="img-fluid h-50" />
+      <img src="./assets/img/404.svg" alt="Page is not founded - error 404" className="img-fluid w-50 h-50" />
       <h1 className="h3">Страница не найдена</h1>
       <p>
-        Давайте вернемся на главную страницу
+        {'Давайте вернемся на '}
+        главную страницу
       </p>
     </div>
   );
