a6568da05d984653598583ef2da343d6c9c80921
kinddoctor <tata-yagodina@yandex.ru>
2024-09-15 17:58:34 +0500

    add routing to app.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
-import ErrorPage from './ErrorPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginForm from './pages/LoginForm';
+import ErrorPage from './pages/ErrorPage';
+import './styles.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App h-100">
       <header className="App-header">
         This is header
       </header>
-      <ErrorPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<ErrorPage />} />
+          <Route path="/" element={<LoginForm />} />
+          <Route path="login" element={<LoginForm />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
