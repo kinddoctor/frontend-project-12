8083d2ecb8fd714b8277e97c87921b7894aa31dc
kinddoctor <tata-yagodina@yandex.ru>
2025-06-23 22:21:18 +0500

    add logout button

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,7 +1,29 @@
-export default function AppHeader() {
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { setData } from '../redux/store/authSlice';
+
+export default function AppHeader({ authorized }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleQuitBtnClick = () => {
+    localStorage.removeItem('ChattyChat token');
+    localStorage.removeItem('ChattyChat username');
+    dispatch(setData({ token: '', username: '' }));
+
+    navigate('/login');
+  };
+
   return (
     <header className="h-auto fs-3 px-5 py-3 border-bottom shadow">
-      Chatty chat
+      <nav className="navbar">
+        <span>Chatty chat</span>
+        {authorized ? (
+          <button onClick={handleQuitBtnClick} type="button" className="btn btn-primary btn-lg">
+            Выйти
+          </button>
+        ) : null}
+      </nav>
     </header>
   );
 }
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,10 +1,13 @@
+import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 import AppHeader from '../components/AppHeader';
 
 export default function Layout() {
+  const authorized = useSelector((state) => state.auth.data.token);
+
   return (
     <div className="h-100">
-      <AppHeader />
+      <AppHeader authorized={authorized} />
       <Outlet />
     </div>
   );
