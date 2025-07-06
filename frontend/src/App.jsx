17eb9005f1cd0ec97e1b72188bc4bbf0943800e7
kinddoctor <tata-yagodina@yandex.ru>
2025-07-06 13:07:56 +0500

    add Rollbar error tracking

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider, ErrorBoundary } from '@rollbar/react';
 import Layout from './pages/Layout';
 import LogIn from './pages/LogIn';
 import SignUp from './pages/SignUp';
@@ -6,18 +7,28 @@ import Chat from './pages/Chat';
 import NotFound from './pages/NotFound';
 import './styles.scss';
 
+const rollbarConfig = {
+  accessToken: import.meta.env.ROLLBAR_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+};
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Chat />} />
-          <Route path="login" element={<LogIn />} />
-          <Route path="signup" element={<SignUp />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider config={rollbarConfig}>
+      <ErrorBoundary>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Chat />} />
+              <Route path="login" element={<LogIn />} />
+              <Route path="signup" element={<SignUp />} />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ErrorBoundary>
+    </Provider>
   );
 }
 
